55f6eec2562f6ed38a547bd21cbee4cd10764e36
Lucas Godina <lucasgodina@gmail.com>
2025-08-19 22:06:24 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'students',  # <-- Agregue app students
+    'base',
+    'users',
+    'careers',
+    'students',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
