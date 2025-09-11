0e7acb3f757bb2b5a779ffc7c26fcd82520f391c
Rocio <rociolujanceleste@gmail.com>
2025-09-11 16:33:34 -0300

    Creado el modelo Subject

--- subjects/models.py
+++ subjects/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.db.models.functions import Lower
 # Create your models here.
+
+class Subject(models.Model):
+    name = models.CharField(
+        max_length=100, 
+        unique=True
+    )
+    description = models.TextField()
+    quota = models.PositiveBigIntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                Lower('name'),
+                name='uq_subject_name_lower'
+            )
+        ]
+        ordering = ['name']
+        
+    def __str__(self):
+        return self.name
