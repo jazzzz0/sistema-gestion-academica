38908bf045ee4109768472610cf648b059c4486b
Lucas Godina <lucasgodina@gmail.com>
2025-09-16 19:27:46 -0300

    Change str

--- enrollments/models.py
+++ enrollments/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Enrollment(models.Model):
+    status_choices = [
+        ("activa", "Activa"),
+        ("aprobada", "Aprobada"),
+        ("reprobada", "Reprobada"),
+        ("ausente", "Ausente"),
+    ]
+
+    student = models.ForeignKey("students.Student", on_delete=models.CASCADE)
+
+    subject = models.ForeignKey("subjects.Subject", on_delete=models.CASCADE)
+
+    enrolled_at = models.DateTimeField(auto_now_add=True)
+
+    status = models.CharField(max_length=10, choices=status_choices, default="activa")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["student", "subject"], name="unique_enrollment"
+            )
+        ]
