1d1b9b6a6d60d8959603199c361f62bb287bd015
Rocio Celeste Lujan <106561540+RocioLujan@users.noreply.github.com>
2025-09-16 18:26:52 -0300

    Creado el modelo career (#13)
    * Creado el modelo career
    
    * Actualizacion modelo careers

--- careers/models.py
+++ careers/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
 
 # Create your models here.
+class Career(models.Model):
+    name = models.CharField(
+        max_length=100, 
+        unique=True,
+        verbose_name="Nombre de la carrera",
+        help_text="Ingrese un nombre único para la carrera."
+    )
+    description = models.TextField(
+        blank=True, 
+        null=True,
+        verbose_name="Descripción",
+        help_text="Opcional: agrege una breve descripcion de la carrera"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Fecha de creación",
+        help_text="Fecha en que se creó la carrera."
+    )
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name="Activo",
+        help_text="Indica si la carrera está activa."
+    )
+    subjects = models.ManyToManyField(
+        "subjects.Subject", 
+        related_name="careers",
+        verbose_name="Materias",
+    )
+    
+    class Meta:
+        ordering = ['name']
+
+    def deactivate(self):
+        if self.subjects.exists() or self.alumnos.exists():
+            raise ValidationError(
+            )
+        self.is_active = False
+        self.save()
+
+    def __str__(self):
+        return self.name
+    
