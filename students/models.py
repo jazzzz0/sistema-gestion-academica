e7a0081bd32a8185553833a581a71a64056093b0
Lucas Godina <lucasgodina@gmail.com>
2025-11-10 21:59:04 -0300

    feat(users): restructure user models and update admin interface

--- students/models.py
+++ students/models.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.db import models
 
-from base.models import Person
+from users.models import Person
 from careers.models import Career
 
 
