72b35cacffdc48facba9f2a2bfc281b27732c762
Lucas Godina <lucasgodina@gmail.com>
2025-11-27 17:49:09 -0300

    Revert "Interfaz y acciones"

--- students/urls.py
+++ students/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = 'students'
-
 urlpatterns = [
     path('', views.StudentListView.as_view(), name='student_list'),
     path('create/', views.StudentCreateView.as_view(), name='student_create'),
