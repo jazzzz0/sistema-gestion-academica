8f3055bcd8b4b9736b2fa5bb3a663bd4e764af5f
jazzzz0 <148421374+jazzzz0@users.noreply.github.com>
2025-11-29 00:02:13 -0300

    Update URL paths to use views directly

--- enrollments/urls.py
+++ enrollments/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from . import views
-from .views import MyEnrollmentListView, EnrollmentDropView
 
 app_name = "enrollments"
 
 urlpatterns = [
     path("", views.inicio, name="inicio"),
-    path("my-enrollments/", MyEnrollmentListView.as_view(), name="my_enrollments"),
-    path("unenroll/<int:pk>/", EnrollmentDropView.as_view(), name="enrollment_drop"),
+    path("my-enrollments/", views.MyEnrollmentListView.as_view(), name="my_enrollments"),
+    path("unenroll/<int:pk>/", views.EnrollmentDropView.as_view(), name="enrollment_drop"),
 ]
