af427306ad82993792f2cde0549c5f9e49d43dfc
jazzzz0 <148421374+jazzzz0@users.noreply.github.com>
2025-11-29 00:26:26 -0300

    Update urls.py

--- students/urls.py
+++ students/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 app_name = 'students'
 
-
 urlpatterns = [
     path('', views.StudentListView.as_view(), name='student_list'),
     path('create/', views.StudentCreateView.as_view(), name='student_create'),
